b5523809e7a2de17a4ed56d579fc61a939523db0
Emmanuel Perez <noel@Noel-2.local>
2024-10-21 17:10:09 +0800

    Added posting application

--- app/Http/Controllers/Posting/PostingApplicationController.php
+++ app/Http/Controllers/Posting/PostingApplicationController.php
@@ -49,9 +49,10 @@ class PostingApplicationController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, PostingApplication $postingApplication)
     {
-        //
+        $data = $postingApplication->update($request->all());
+        return $this->sendResponse($request->all(), 'Application successfully updated.');
     }
 
     /**

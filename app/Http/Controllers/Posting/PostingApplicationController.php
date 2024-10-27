9751153b7828d4607af55637214c354e831099a9
Mark Santos <christian.santos37@gmail.com>
2024-10-27 18:09:22 +0800

    update to base controller

--- app/Http/Controllers/Posting/PostingApplicationController.php
+++ app/Http/Controllers/Posting/PostingApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Posting;
 
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Posting\PostingApplicationRequest;
 use App\Http\Resources\Posting\PostingApplicationResource;
@@ -9,7 +10,7 @@ use App\Models\Posting\PostingApplication;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
 
-class PostingApplicationController extends Controller
+class PostingApplicationController extends  BaseController
 {
     /**
      * Display a listing of the resource.
@@ -29,12 +30,22 @@ class PostingApplicationController extends Controller
                 }]); */
             })
             ->when(isset($request->show_list), function ($query) use ($request) {
-                $query->where('posting_id', $request->posting_id)->with(['user']);
+                $query->where('posting_id', $request->posting_id)
+                ->with(['user', 'user.school', 'user.parents', 'user.academicProgram', 'user.yearLevel', 'user.parents.monthlyIncome', 'user.userEducation', 'user.userEducation.educationLevel', 'user.userEducation.academicProgram', 'user.employment', 'user.reference', 'user.skill']);
+            })
+            ->when(isset($request->application_status) && $request->application_status === 'pending', function ($q) use ($request) {
+                $q->whereNull('is_approved');
+            })
+            ->when(isset($request->application_status) && $request->application_status === 'verified', function ($q) use ($request) {
+                $q->where('is_approved', 1);
+            })
+            ->when(isset($request->application_status) && $request->application_status === 'rejected', function ($q) use ($request) {
+                $q->where('is_approved', 0);
             })
             ->when(!auth()->user()->is_admin, function($query) use($request) {
                 $query->whereUserId(auth()->id());
             })
-            ->allowedIncludes(['updatedBy', 'posting', 'user'])
+            ->allowedIncludes(['updatedBy', 'posting', 'user', 'user.school'])
             ->defaultSort('date_applied')
             ->allowedSorts(['date_applied']);
         if ($perPage === 'all') {
@@ -67,7 +78,7 @@ class PostingApplicationController extends Controller
     public function update(Request $request, PostingApplication $postingApplication)
     {
         $data = $postingApplication->update($request->all());
-        return $this->sendResponse($request->all(), 'Application successfully updated.');
+        return $this->sendResponse($data, 'Application successfully updated.');
     }
 
     /**

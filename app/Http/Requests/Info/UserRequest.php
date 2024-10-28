2bf4bb74f8d3e538ec5e639459416487586abd54
Mark Santos <christian.santos37@gmail.com>
2024-10-28 18:53:22 +0800

    update user request

--- app/Http/Requests/Info/UserRequest.php
+++ app/Http/Requests/Info/UserRequest.php
@@ -62,8 +62,9 @@ class UserRequest extends FormRequest
             'shiftee_flag' => 'nullable|boolean',
             'irregular_flag' => 'nullable|boolean',
 
-            'lib_education_level_id' => 'nullable|exists:lib_education_levels,id',
-            'lib_academic_program_id' => 'required|exists:lib_academic_programs,id'
+            'lib_education_level_id' => 'required|exists:lib_education_levels,id',
+            'lib_academic_program_id' => 'required|exists:lib_academic_programs,id',
+            'lib_year_level_id' => 'required|exists:lib_year_levels,id'
         ];
     }
 }
